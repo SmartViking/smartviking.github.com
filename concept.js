bcf4bc03a4bc6e40c8b291406636c2fa9a4a490f
Smart Viking <smartestviking@gmail.com>
2012-01-29 22:06:15 +0100

    nothing

--- concept.js
+++ concept.js
@@ -2,11 +2,20 @@ $(document).ready( function() {
 	
 	$("#input").focus();
 	
+	$(document).click( function() {
+		$("#input").focus();
+	});
+	
 	$("#input").keyup( function(event) {
 
 	if (event.keyCode == 13) {
 		var x = $("#dump")
-		switch(this.value)
+		var t = this.value
+		var findReplace = [[/&/g, "&amp;"], [/</g, "&lt;"], [/>/g, "&gt;"], [/"/g, "&quot;"]]
+		for(var item in findReplace)
+			t = t.replace(findReplace[item][0], findReplace[item][1]);
+
+		switch(t)
 		{
 		case "clear":
 			x.text("");
@@ -15,10 +24,10 @@ $(document).ready( function() {
 			x.append("<span class=\"commands\">$</span><br>");
 			break;
 		case "halp": case "help":
-			x.append("<span class=\"commands\">$"+this.value+"<br>HAYYY!</span><br>");
+			x.append("<span class=\"commands\">$"+t+"<br>HAYYY!</span><br>");
 			break;
 		default:
-		x.append("<span class=\"commands\">$"+this.value+"<br>"+this.value+": command not found</span><br>");
+		x.append("<span class=\"commands\">$"+t+"<br>"+t+": command not found</span><br>");
 		}
 		this.value = "";
 		window.location = "#input";
